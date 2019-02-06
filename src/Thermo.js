c065416e7645e531ded97b51fa023f665005b677
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-06 21:35:52 +0000

    Added powersaving method and changed up method - passing the test

--- src/Thermo.js
+++ src/Thermo.js
@@ -1,6 +1,7 @@
 function Thermo(){
   this.def_temp = 20;
   this.min_temp = 10;
+  this.max_temp = 25;
 };
 
 Thermo.prototype.temperature = function(){
@@ -8,9 +9,18 @@ Thermo.prototype.temperature = function(){
 };
 
 Thermo.prototype.up = function(){
-  this.def_temp++
+  if (this.def_temp < this.max_temp ) {
+    this.def_temp++
+  } else {
+    return this.def_temp;
+  };
 };
 
 Thermo.prototype.down = function(){
   this.def_temp -= 1;
+};
+
+Thermo.prototype.powersaving_on = function (){
+  this.max_temp = 25;
+  return this.max_temp;
 };
